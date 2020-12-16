dd41a5968e351f95035395cb4ee76c2066a7e42d
javi navarro vega <lobosaur22@gmail.com>
2020-12-16 10:39:30 +0100

    hola1

---
+++ src/paquete/Principal.java
@@ -0,0 +1,12 @@
+package paquete;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola Mundo");
+		System.out.println("hola hola");
+	}
+
+}
